d4d198bf4279bc755a6b7fb324be75cdd715a1d9
Sudheshna Jinna <sudheshnareddy1720@gmail.com>
2023-09-14 18:32:01 -0400

    navbar changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,20 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
+import Scrollspy from 'react-scrollspy'
 import { Link } from 'react-scroll';
 import sj from '../assets/img/sj.png';
+//import navIcon1 from 'https://assets4.lottiefiles.com/packages/lf20_w51pcehl.json';
 import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/nav-icon3.svg';
 import navIcon4 from '../assets/img/nav-icon4.png';
 import { HashLink } from 'react-router-hash-link';
-import './NavBar.css'; // Import your CSS file for styling
+import {
+  BrowserRouter as Router
+} from "react-router-dom";
 
-const NavBar = () => {
+export const NavBar = () => {
+
   const [activeLink, setActiveLink] = useState('home');
   const [scrolled, setScrolled] = useState(false);
 
@@ -30,80 +37,39 @@ const NavBar = () => {
   }
 
   return (
-    <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
-      <Container>
-        <Navbar.Brand href="/">
-          <div className="logo">
-            <img className="sj" src={sj} alt="Hello viewer!!" />
-            &nbsp;
-            <h1>SUDHESHNA</h1>
-          </div>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav">
-          <span className="navbar-toggler-icon"></span>
-        </Navbar.Toggle>
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            <Link
-              to="home"
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration={500}
-              className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'}
-              onClick={() => onUpdateActiveLink('home')}
-            >
-              Home
-            </Link>
-            <Link
-              to="skills"
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration={500}
-              className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'}
-              onClick={() => onUpdateActiveLink('skills')}
-            >
-              Skills
-            </Link>
-            <Link
-              to="projects"
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration={500}
-              className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'}
-              onClick={() => onUpdateActiveLink('projects')}
-            >
-              Projects
-            </Link>
-            <a
-              href="https://drive.google.com/file/d/1yM95PgO6q_db8RtzbNhHJMUVxnHlpxJ7/view?usp=sharing"
-              target="_blank"
-              rel="noopener noreferrer"
-              className={activeLink === 'resume' ? 'active navbar-link' : 'navbar-link'}
-            >
-              Resume
-            </a>
-          </Nav>
-          <span className="navbar-text">
-            <div className="social-icon">
-              <a href="https://www.linkedin.com/in/sudheshna-jinna-71b0481b7/" target="_blank">
-                <img src={navIcon1} alt="LinkedIn" />
-              </a>
-              <a href="https://github.com/sudheshnajinna" target="_blank">
-                <img src={navIcon4} alt="GitHub" />
-              </a>
-              <a href="#"><img src={navIcon3} alt="" /></a>
+    <Router>
+      <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
+        <Container>
+          <Navbar.Brand href="/">
+            <div className="logo">
+              <img className="sj" src={sj} alt="Hello viewer!!" />
+              &nbsp;
+              <h1>SUDHESHNA</h1>
             </div>
-            <HashLink to='#connect'>
-              <button className="vvd"><span>Let’s Connect</span></button>
-            </HashLink>
-          </span>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav">
+            <span className="navbar-toggler-icon"></span>
+          </Navbar.Toggle>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+              <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
+              <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+              <Nav.Link href="https://drive.google.com/file/d/1yM95PgO6q_db8RtzbNhHJMUVxnHlpxJ7/view?usp=sharing" target="_blank" rel="noopener noreferrer" className={activeLink === 'resume' ? 'active navbar-link' : 'navbar-link'}>Resume</Nav.Link>
+            </Nav>
+            <span className="navbar-text">
+              <div className="social-icon">
+                <a href="https://www.linkedin.com/in/sudheshna-jinna-71b0481b7/" target="_blank"><img src={navIcon1} alt="" /></a>
+                <a href="https://github.com/sudheshnajinna" target="_blank"><img src={navIcon4} alt="" /></a>
+                <a href="#"><img src={navIcon3} alt="" /></a>
+              </div>
+              <HashLink to='#connect'>
+                <button className="vvd"><span>Let’s Connect</span></button>
+              </HashLink>
+            </span>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </Router>
+  )
 }
-
-export default NavBar;
